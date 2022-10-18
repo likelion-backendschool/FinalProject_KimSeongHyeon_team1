39d0eaa108a816d950e5834bf576441b79c2efc6
castlehyeon <qnftktls118@naver.com>
2022-10-19 02:18:32 +0900

    feat: Securityconfig 설정 #1
    - config-security 패키지 내 클래스 생성

---
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/config/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.mutbooks.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//특정 주소로 접근하면 권한 및 인증을 미리 체크하기 위해 사용
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final MemberLoginService memberLoginService;
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberLoginService).passwordEncoder(encoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+//        static 하위 폴더 (css, js, img)는 무조건 접근이 가능해야하기 때문에 인증을 무시할 경로를 설정
+        web
+                .ignoring().antMatchers( "/css/**", "/js/**", "/img/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/auth/**", "/posts/read/**", "/posts/search/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("loginProc")
+                .defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true);
+    }
+}
+
