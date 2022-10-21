3b373dfbbf12a2a7286845df5745efd05f652351
castlehyeon <qnftktls118@naver.com>
2022-10-21 09:27:45 +0900

    pr을 위한 프로젝트 파일 삭제

--- 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/domain/post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mutbooks.domain.post.repository;
-
-import com.example.mutbooks.domain.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
