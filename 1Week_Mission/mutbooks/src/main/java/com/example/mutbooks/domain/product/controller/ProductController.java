875914d502bc212e1b2a79ba9b54b2bef4b081b6
castlehyeon <qnftktls118@naver.com>
2022-10-26 04:49:09 +0900

    feat:  도서목록 구현
    - 도서 상세보기 dto, service, controller 작성

--- 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/domain/product/controller/ProductController.java
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/domain/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.mutbooks.domain.product.controller;
 
+import com.example.mutbooks.domain.product.dto.ProductDetailFormDto;
 import com.example.mutbooks.domain.product.entity.Product;
 import com.example.mutbooks.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,5 +25,17 @@ public class ProductController {
         model.addAttribute("productList", productList);
 
         return "product/product_list";
+    }
+
+    /* 도서 상세보기 */
+    @GetMapping("/product/{id}")
+    public String showDetail(@PathVariable Long id, Model model) {
+        Product product = productService.findById(id);
+        model.addAttribute("id", id);
+
+
+        ProductDetailFormDto productDetailFormDto = productService.getProductFormById(product);
+        model.addAttribute("product", productService.detail(id));
+        return "post/post_detail";
     }
 }
--- 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/domain/product/service/ProductService.java
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/domain/product/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.example.mutbooks.domain.product.service;
 
+import com.example.mutbooks.domain.product.dto.ProductDetailFormDto;
 import com.example.mutbooks.domain.product.entity.Product;
 import com.example.mutbooks.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,21 @@ public class ProductService {
     public List<Product> findAll() {
         List<Product> findProduct = productRepository.findAll();
         return findProduct;
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> findProduct =  productRepository.findById(id);
+        return findProduct.get();
+    }
+
+    public ProductDetailFormDto getProductFormById(Product product) {
+            ProductDetailFormDto productDetailFormDto = ProductDetailFormDto.builder()
+                    .name(product.getName())
+                    .imgUrl(product.getImgUrl())
+                    .description(product.getDescription())
+                    .price(product.getPrice())
+                    .build();
+
+            return productDetailFormDto;
     }
 }
