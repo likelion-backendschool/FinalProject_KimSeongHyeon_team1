65f17e21e5466db7a351e9370195712962f8ce4a
castlehyeon <qnftktls118@naver.com>
2022-11-02 11:21:51 +0900

    feat:  정산 전체, 건별 뷰
    - revenue 전체, 건별 뷰 작성

--- 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/domain/revenue/controller/RevenueController.java
+++ 1Week_Mission/mutbooks/src/main/java/com/example/mutbooks/domain/revenue/controller/RevenueController.java
@@ -38,7 +38,7 @@ public class RevenueController {
         return OrderType.values();
     }
 
-    /*주문내역 페이지*/
+    /*주문내역전체 페이지*/
     @GetMapping("{username}/adm/rebate/rebateOrderItemList")
     public String showTotalOrderList(Model model, @PathVariable String username){
         List<Order> orderLists = orderService.findAllUsername(username);
@@ -47,63 +47,12 @@ public class RevenueController {
     }
 
 
-    /*주문내역 상세 페이지*/
+    /*주문내역 건별 페이지*/
     @GetMapping("/{username}/adm/rebate/rebateOrderItemList/{orderId}")
     public String showTotalOrderDetail(Model model , @PathVariable String username, @PathVariable Long orderId){
         /*해당 id의 order 객체만을 가져옴*/
         Order findOrder = orderService.findById(orderId);
         model.addAttribute("findOrder", findOrder);
         return "revenue/orderDetail";
-    }
-
-    /*매출관리 페이지*/
-    @GetMapping("/{storeSN}/admin/store/management/revenue")
-    public String showTotalSalesList(Model model , @PathVariable String storeSN){
-        Store store = storeService.findBySerialNumber(storeSN);
-        model.addAttribute("store", store);
-        model.addAttribute("storeSN" , storeSN);
-        /*스토어넘버로 모든 주문 내역을 가져온다. order 테이블의 모든 것을 조회하기 위해 객체를 가져옴*/
-        List<Order> orderLists = orderService.findAllOrderByStoreSN(storeSN);
-        model.addAttribute("orderLists", orderLists);
-        return "storeMgmt/revenueList";
-    }
-
-    /* 메뉴관리 페이지 */
-    @GetMapping("/{storeSN}/admin/store/management/menu")
-    public String showMenuMgmt(Model model , @PathVariable String storeSN) {
-        Store store = storeService.findBySerialNumber(storeSN);
-        model.addAttribute("store", store);
-        return "storeMgmt/mgmtMenu";
-    }
-
-    /*매출관리 정렬 페이지*/
-    @GetMapping("/{storeSN}/admin/store/management/revenue/{orderBy}")
-    public String showTotalSalesList(Model model , @PathVariable String storeSN, @PathVariable String orderBy){
-        Store store = storeService.findBySerialNumber(storeSN);
-        model.addAttribute("store", store);
-        model.addAttribute("storeSN" , storeSN);
-
-        /*정렬방법에 따라 SQL로 원하는 값을 가져온다.*/
-        switch (orderBy) {
-            case "price":
-                List<FindListFormDto> findList1 = orderService.getFindListFormDtoListForPriceDaily(storeSN);
-                //맵으로 정렬
-                Map<String, Long> findList = orderService.calculateSumForDay(findList1);
-                //최신순 정렬
-                Map<String, Long> treeMap = new TreeMap<>(Collections.reverseOrder());
-                treeMap.putAll(findList);
-                model.addAttribute("findList", treeMap);
-                break;
-            case "user":
-                List<FindByUserDailyDto> findList2 = orderService.getFindByUserDailyDtoListForUserDaily(storeSN);
-                //맵으로 정렬
-                Map<String, Long> findList3 = orderService.calculateSumForUser(findList2);
-                //최신순 정렬
-                Map<String, Long> treeMap2 = new TreeMap<>(Collections.reverseOrder());
-                treeMap2.putAll(findList3);
-                model.addAttribute("findList3", treeMap2);
-                break;
-        }
-        return "storeMgmt/revenueDetail";
     }
 }
